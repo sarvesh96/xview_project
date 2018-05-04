e2f66a27fe5bc8eac8989045df22d80234553969
Ubuntu <ubuntu@ip-172-31-16-7.us-west-2.compute.internal>
2018-05-04 00:50:31 +0000

    test for loss

--- ssd/layers/modules/multibox_loss.py
+++ ssd/layers/modules/multibox_loss.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
+import time
 import torch
+import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -87,6 +89,15 @@ class MultiBoxLoss(nn.Module):
 		pos_idx = pos.unsqueeze(pos.dim()).expand_as(loc_data)
 		loc_p = loc_data[pos_idx].view(-1, 4)
 		loc_t = loc_t[pos_idx].view(-1, 4)
+		print("LOC_P")
+		print(loc_p)
+		print("LOC_T")
+		print(loc_t)
+		print("TARGETS")
+		print(targets)
+		print("PREDS")
+		print(predictions)
+		time.sleep(20)
 		loss_l = F.smooth_l1_loss(loc_p, loc_t, size_average=False)
 
 		# Compute max conf across batch for hard negative mining
