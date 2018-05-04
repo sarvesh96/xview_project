8452f5935d3bb842276a6854ad5c21557e56f5be
Ubuntu <ubuntu@ip-172-31-16-7.us-west-2.compute.internal>
2018-05-04 05:54:27 +0000

    updating comments

--- ssd/layers/modules/multibox_loss.py
+++ ssd/layers/modules/multibox_loss.py
@@ -91,8 +91,8 @@ class MultiBoxLoss(nn.Module):
 		loc_t = loc_t[pos_idx].view(-1, 4)
 
 		# Check for images with no boxes or classes and remove them from loss calculation
-		loc_p = loc_p[loc_t != -float('inf')]
-		loc_t = loc_t[loc_t != -float('inf')]
+		# loc_p = loc_p[loc_t != -float('inf')]
+		# loc_t = loc_t[loc_t != -float('inf')]
 
 		loss_l = F.smooth_l1_loss(loc_p, loc_t, size_average=False)
 
@@ -101,8 +101,8 @@ class MultiBoxLoss(nn.Module):
 		loss_c = log_sum_exp(batch_conf) - batch_conf.gather(1, conf_t.view(-1, 1))
 
 		# Hard Negative Mining
-		# loss_c = loss_c.view(num, -1)
 		loss_c[pos.view(-1, 1)] = 0  # filter out pos boxes for now
+		#loss_c[pos] = 0  # filter out pos boxes for now
 		loss_c = loss_c.view(num, -1)
 		_, loss_idx = loss_c.sort(1, descending=True)
 		_, idx_rank = loss_idx.sort(1)
